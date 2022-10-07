2bc963b814e45a23ecd9c531ed5b1d4728ad2770
Edwin Ayerbe <edwin.ayerbe@DOMINIO.local>
2022-10-07 16:22:41 -0500

    add logo

--- src/components/Navar/Navbar.js
+++ src/components/Navar/Navbar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import CartWidget from '../CartWidget/CartWidget';
-import './Navbar.css'
+import './Navbar.css';
+import Logo from './../../Media/Imgs/Logo.png'
 
 const Navbar = () => {
     return(
         <nav className="NavBar">
-            <h1 className='Title'>Ecommerce</h1>
+            <Link className="Title" to='/'>
+                <img className="img" src={Logo} alt="Logo" />
+               
+            </Link>
             <div className="Buttons">
                 <Link  className="Option" to={'/category/celular'}>Celulares</Link>
                 <Link className="Option" to={'/category/tablet'}>Tablets</Link>
